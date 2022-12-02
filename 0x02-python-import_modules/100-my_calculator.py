f26b9678e5bd4c484f3d0c5a5c2d0e4a5790b064
Memenzy <mmormoni75@gmail.com>
2022-12-02 01:01:11 +0000

    corr Adv tasks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,47 +1,34 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
 
-    import sys
+    """Handle basic arithmetic operations."""
 
     from calculator_1 import add, sub, mul, div
 
-    argv = sys.argv[1:]
+    import sys
 
-    argv_count = len(argv)
 
-    operators = ["+", "-", "*", "/"]
 
-    if argv_count is not 3:
+    if len(sys.argv) - 1 != 3:
 
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
 
-        exit(1)
+        sys.exit(1)
 
-    elif sys.argv[2] not in operators:
+
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    if sys.argv[2] not in list(ops.keys()):
 
         print("Unknown operator. Available operators: +, -, * and /")
 
-        exit(1)
+        sys.exit(1)
 
-    else:
 
-        a = int(sys.argv[1])
 
-        b = int(sys.argv[3])
+    a = int(sys.argv[1])
 
-        if sys.argv[2] is "+":
+    b = int(sys.argv[3])
 
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-
-        elif sys.argv[2] is "-":
-
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-
-        elif sys.argv[2] is "*":
-
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-
-        elif sys.argv[2] is "/":
-
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
