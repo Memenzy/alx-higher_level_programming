f2278c6be7f24a6b2b1a5c73147a003f3ef71bcc
Memenzy <mmormoni75@gmail.com>
2022-12-10 01:48:23 +0000

    all done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,91 +1,56 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
 
-    # Fail checks, none, not a string
+    """Converts a roman numeral to an integer."""
 
-    if not roman_string:
+    if (not isinstance(roman_string, str) or
 
-        return 0
+            roman_string is None):
 
-    if not isinstance(roman_string, str):
+        return (0)
 
-        return 0
 
-    if not roman_string.isupper():
 
-        return 0
+    roman_dict = {
 
-    # Dictionary for roman numerals
+            "I": 1,
 
-    r_dict = {
+            "V": 5,
 
-        "I": 1,
+            "X": 10,
 
-        "IV": 4,
+            "L": 50,
 
-        "V": 5,
+            "C": 100,
 
-        "IX": 9,
+            "D": 500,
 
-        "X": 10,
-
-        "L": 50,
-
-        "C": 100,
-
-        "D": 500,
-
-        "M": 1000
+            "M": 1000
 
     }
 
+    num = 0
 
 
-    result = 0
 
-    temp = list(roman_string)
+    for i in range(len(roman_string)):
 
-    # Concat 4 and 9s
+        if roman_dict.get(roman_string[i], 0) == 0:
 
-    if len(temp) > 1:
-
-        idx = 0
-
-        for i in temp:
-
-            try:
-
-                if temp[idx] == 'I' and temp[idx + 1] == 'V':
-
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-
-            except IndexError:
-
-                pass
-
-            try:
-
-                if temp[idx] == 'I' and temp[idx + 1] == 'X':
-
-                    temp[idx:idx + 2] = [''.join(temp[idx:idx + 2])]
-
-            except IndexError:
-
-                pass
-
-            idx += 1
-
-    # Search in dict for correct numbers and add
-
-    for k, v in r_dict.items():
-
-        for index in temp:
-
-            if index == k:
-
-                result += v
-
-    return result
+            return (0)
 
 
+
+        if (i != (len(roman_string) - 1) and
+
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
+
+                num += roman_dict[roman_string[i]] * -1
+
+
+
+        else:
+
+            num += roman_dict[roman_string[i]]
+
+    return (num)
